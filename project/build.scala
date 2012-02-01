2cb371c0cca8e1e9011f6a00b91ecaae8cba7f82
Josh Suereth <joshua.suereth@gmail.com>
2012-02-01 16:46:23 -0500

    Fixes to debian package.

---
+++ project/build.scala
@@ -0,0 +1,7 @@
+import sbt._
+import Keys._
+
+object GiterateBuild extends Build {
+  // This build creates a SBT plugin with handy features *and* bundles the SBT script for distribution.
+  val root = Project("giter8-package", file(".")) settings(Packaging.settings:_*)
+}
