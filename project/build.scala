107d7667a43de97b99e24fccb31fccec6fa436c0
Ricky Elrod <ricky@elrod.me>
2013-05-16 00:00:57 -0300

    Fix a missing version bump for 0.5.3

--- project/build.scala
+++ project/build.scala
@@ -3,5 +3,5 @@ import Keys._
 
 object GiterateBuild extends Build {
   // This build creates a SBT plugin with handy features *and* bundles the SBT script for distribution.
-  val root = Project("giter8-package", file(".")) settings(Packaging.settings:_*) settings(version := "0.4.5")
+  val root = Project("giter8-package", file(".")) settings(Packaging.settings:_*) settings(version := "0.5.3")
 }
